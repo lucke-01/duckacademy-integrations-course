f26bc5dbdf2c56c3809701b51d61f23e4516b51e
Ricardo <ricardo@ricardocreates.com>
2025-02-07 18:04:26 +0100

    Added websocket notification

--- websocket/src/main/java/duckacademy/nl/websocket/ws/SocketHandler.java
+++ websocket/src/main/java/duckacademy/nl/websocket/ws/SocketHandler.java
@@ -2,12 +2,15 @@ package duckacademy.nl.websocket.ws;
 
  import org.slf4j.Logger;
  import org.slf4j.LoggerFactory;
+ import org.springframework.scheduling.annotation.Scheduled;
  import org.springframework.stereotype.Component;
  import org.springframework.web.socket.TextMessage;
  import org.springframework.web.socket.WebSocketSession;
  import org.springframework.web.socket.handler.TextWebSocketHandler;
 
  import java.io.IOException;
+ import java.text.SimpleDateFormat;
+ import java.util.Date;
  import java.util.List;
  import java.util.Map;
  import java.util.concurrent.CopyOnWriteArrayList;
@@ -20,6 +23,15 @@ public class SocketHandler extends TextWebSocketHandler {
     Logger logger = LoggerFactory.getLogger(SocketHandler.class);
     List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
+    @Scheduled(fixedRate = 5000)
+    public void notifyTime() throws IOException {
+        logger.info("IMPORTANT notifying time");
+        String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        for(WebSocketSession webSocketSession : sessions) {
+            logger.info("IMPORTANT sending messages to all sessions");
+            webSocketSession.sendMessage(new TextMessage("TIME IS "+ time));
+        }
+    }
     //We receive a message
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message)

f7c91c4b753d93cf53879b93f8570ba9143474b5
Ricardo <ricardo@ricardocreates.com>
2025-02-11 09:34:57 +0100

    rest-api testing done

--- rest-api/src/main/java/nl/duckacademy/rest_api/rest/ExceptionManager.java
+++ rest-api/src/main/java/nl/duckacademy/rest_api/rest/ExceptionManager.java
@@ -2,21 +2,12 @@ package nl.duckacademy.rest_api.rest;
 
 import com.swagger.client.codegen.rest.model.GenericErrorDTO;
 import nl.duckacademy.rest_api.customexception.UserNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionManager {
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<GenericErrorDTO> handleIllegalArgument(IllegalArgumentException exception) {
-        GenericErrorDTO genericErrorDTO = new GenericErrorDTO();
-        genericErrorDTO.setErrorCode(HttpStatus.NOT_FOUND.name());
-        genericErrorDTO.setMessage(exception.getMessage());
-        genericErrorDTO.setErrorCode("");
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(genericErrorDTO);
-    }
     @ExceptionHandler(UserNotFoundException.class)
     protected ResponseEntity<GenericErrorDTO> handleIllegalArgument(UserNotFoundException exception) {
         GenericErrorDTO genericErrorDTO = new GenericErrorDTO();
---
+++ rest-api/src/test/java/nl/duckacademy/rest_api/utiltest/HttpUtilTest.java
@@ -0,0 +1,18 @@
+package nl.duckacademy.rest_api.utiltest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpUtilTest {
+    @Autowired
+    private ObjectMapper objectMapper;
+    public String asJsonString(final Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

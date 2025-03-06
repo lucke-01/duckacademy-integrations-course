1c83c800657f50165360d358c76c550d4469f7b6
Ricardo <ricardo@ricardocreates.com>
2025-03-06 12:40:16 +0100

    gRPC > configuration and helloWorld

---
+++ gRPC/server/src/main/java/nl/duckacademy/services/HelloServiceImpl.java
@@ -0,0 +1,24 @@
+package nl.duckacademy.services;
+
+
+
+import io.grpc.stub.StreamObserver;
+import nl.duckacademy.gRPC_server.grpc.HelloRequest;
+import nl.duckacademy.gRPC_server.grpc.HelloResponse;
+import nl.duckacademy.gRPC_server.grpc.HelloServiceGrpc;
+
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        System.out.println("Request received from client:\n" + request);
+        String greeting = new StringBuilder()
+                .append("Hello, ").append(request.getFirstName()).append(" ")
+                .append(request.getLastName()).toString();
+
+        HelloResponse response = HelloResponse.newBuilder().setGreeting(greeting).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}

ee20d9d721bd252b0ea89e16fc15e9dee93fbc83
ricardo <ricardojara01@gmail.com>
2025-03-19 13:39:11 +0100

    Removed EJB

--- EJB/common/src/main/java/nl/duckacademy/common/ejb/HelloWorldEJBRemote.java
+++
@@ -1,8 +0,0 @@
-package nl.duckacademy.common.ejb;
-
-import jakarta.ejb.Remote;
-
-@Remote
-public interface HelloWorldEJBRemote {
-    public String helloWorld(String name);
-}
--- EJB/server/src/main/java/nl/duckacademy/Main.java
+++
@@ -1,7 +0,0 @@
-package nl.duckacademy;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
